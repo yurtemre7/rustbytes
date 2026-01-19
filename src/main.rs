4956a4bedbda342c5d8da812cbb3d852f94737dc
Emre Yurtseven <emreyurtseven18@gmail.com>
2026-01-19 17:21:13 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::io::{BufWriter, stdout};
+
+use ferris_says::say;
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
+}
