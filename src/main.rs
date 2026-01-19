a72cbd4a5fe4e92063296b3f01f095a6d46a9119
Emre Yurtseven <emreyurtseven18@gmail.com>
2026-01-19 18:27:10 +0100

    tests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,80 @@
-use std::io::{BufWriter, stdout};
+fn greet(name: &str) {
+    println!("Hello {}!", name);
+}
 
-use ferris_says::say;
+fn fair_dice_roll() -> i32 {
+    4
+}
+
+struct Vec2 {
+    x: f64,
+    y: f64,
+}
+
+struct Zahl {
+    ungerade: bool,
+    wert: i32,
+}
+
+impl Zahl {
+    fn is_strictly_positive(self) -> bool {
+        self.wert > 0
+    }
+}
+
+trait Signed {
+    fn is_strictly_negative(self) -> bool;
+}
+
+impl Signed for Zahl {
+    fn is_strictly_negative(self) -> bool {
+        self.wert < 0
+    }
+}
+
+fn print_zahl(z: &Zahl) {
+    match z.wert {
+        1 => println!("Eins"),
+        2 => println!("Zwei"),
+        _ => println!("{}", z.wert),
+    }
+}
+
+fn file_ext(name: &str) -> Option<&str> {
+    // this does not create a new string - it returns
+    // a slice of the argument.
+    name.split(".").last()
+}
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    let emre = String::from("Emre");
+    let x = vec![1, 2, 3].iter().map(|x| x + 3).fold(0, |x, y| x + y);
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
+    println!("The sum is {}", x);
+    greet(&emre);
+    greet(&emre);
+
+    let v1 = Vec2 { x: 1.0, y: 3.0 };
+
+    let one = Zahl {
+        ungerade: true,
+        wert: 1,
+    };
+    let two = Zahl {
+        ungerade: false,
+        wert: 2,
+    };
+
+    let minus_two = Zahl {
+        ungerade: false,
+        wert: -2,
+    };
+
+    print_zahl(&one);
+    print_zahl(&two);
+    println!("negative? {}", minus_two.is_strictly_negative());
+
+    let name = String::from("Read me. Or don't.txt");
+    let ext = { file_ext(&name).unwrap_or("") };
+    println!("extension: {:?}", ext);
 }
